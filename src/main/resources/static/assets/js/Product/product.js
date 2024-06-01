6e39baf1944f842dfb56f017edb6697693dea30f
Saranya121 <saranyagajendran30@gmail.com>
2024-06-01 07:24:01 +0530

    js commit

--- src/main/resources/static/assets/js/Product/product.js
+++ src/main/resources/static/assets/js/Product/product.js
@@ -2,7 +2,7 @@ function switchPage(value) {
 	if (value == 'Add') {
 		window.location.href = "/product/add";
 	} else if (value == 'Back') {
-		window.location.href = "/product/index";
+		window.location.href = "/product";
 	} else if (value == 'Display') {
 		window.location.href = "/product/displayAll";
 	}
